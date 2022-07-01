d97318d6994689cc0109b1a3fce1c6d3098ad4ea
Gaelan Venturi <gaelan.venturi@nhrec.org>
2022-06-30 23:22:30 -0400

    Transparency is default now

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -64,6 +64,22 @@ return packer.startup(function(use)
   use "lunarvim/darkplus.nvim"
   use 'eddyekofo94/gruvbox-flat.nvim'
   use 'xiyaowong/nvim-transparent'
+  require("transparent").setup({
+  enable = true, -- boolean: enable transparent
+  extra_groups = { -- table/string: additional groups that should be cleared
+    -- In particular, when you set it to 'all', that means all available groups
+
+    -- example of akinsho/nvim-bufferline.lua
+    "BufferLineTabClose",
+    "BufferlineBufferSelected",
+    "BufferLineFill",
+    "BufferLineBackground",
+    "BufferLineSeparator",
+    "BufferLineIndicatorSelected",
+  },
+  exclude = {}, -- table: groups you don't want to clear
+})
+
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
